4995cb0d234163eeb4e80a6818773c95d1bf5c36
Soham Mukherjee <mukherjee.126@buckeyemail.osu.edu>
2018-12-05 02:30:29 -0500

    Added Plot

--- Homework 5/EM.py
+++ Homework 5/EM.py
@@ -1,28 +1,23 @@
-# EM.py
-# --------------
-# Licensing Information:  You are free to use or extend this project for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to The Ohio State University, including a link to http://aritter.github.io/courses/5523_fall18.html
-#
-# Attribution Information: This assignment was developed at The Ohio State University
-# by Alan Ritter (ritter.1492@osu.edu).
+#!/usr/bin/python
+
+#########################################################
+# CSE 5523 starter code (HW#5)
+# Alan Ritter
+# submitted by Rupen Mitra
+#########################################################
 
 import random
 import math
 import sys
 import re
-import numpy as np
+import matplotlib.pyplot as plt
 
-#GLOBALS/Constants
+# GLOBALS/Constants
 VAR_INIT = 1
 
 
 def logExpSum(x):
-    xmax = np.max(x)
-    val = x - xmax
-    val = np.sum(np.exp(val))
-    return xmax + np.log(val)
+    return max(x) + math.log(sum(math.exp(xi - max(x)) for xi in x))
 
 
 def readTrue(filename='wine-true.data'):
@@ -33,8 +28,9 @@ def readTrue(filename='wine-true.data'):
         labels.append(int(splitRe.split(line)[0]))
     return labels
 
+
 #########################################################################
-#Reads and manages data in appropriate format
+# Reads and manages data in appropriate format
 #########################################################################
 class Data:
     def __init__(self, filename):
@@ -44,7 +40,7 @@ class Data:
         for line in f:
             self.data.append([float(x) for x in line.split(" ")])
 
-    #Computers the range of each column (returns a list of min-max tuples)
+    # Computers the range of each column (returns a list of min-max tuples)
     def Range(self):
         ranges = []
         for j in range(self.nCols):
@@ -61,128 +57,170 @@ class Data:
     def __getitem__(self, row):
         return self.data[row]
 
+
 #########################################################################
-#Computes EM on a given data set, using the specified number of clusters
-#self.parameters is a tuple containing the mean and variance for each gaussian
+# Computes EM on a given data set, using the specified number of clusters
+# self.parameters is a tuple containing the mean and variance for each gaussian
 #########################################################################
 class EM:
     def __init__(self, data, nClusters):
         # Initialize parameters randomly...
         random.seed()
         self.parameters = []
-        self.priors = []        # Cluster priors
+        self.priors = []  # Cluster priors
         self.nClusters = nClusters
         self.data = data
         ranges = data.Range()
-        self.resp = np.zeros((nClusters, self.data.nRows))
         for i in range(nClusters):
             p = []
-            initRow = random.randint(0, data.nRows-1)
+            initRow = random.randint(0, data.nRows - 1)
             for j in range(data.nCols):
-                # Randomly initalize variance in range of data
-                p.append((random.uniform(ranges[j][0], ranges[j][1]), VAR_INIT*(ranges[j][1] - ranges[j][0])))
+                # Randomly initialize variance in range of data
+                p.append((random.uniform(ranges[j][0], ranges[j][1]), VAR_INIT * (ranges[j][1] - ranges[j][0])))
             self.parameters.append(p)
 
         # Initialize priors uniformly
         for c in range(nClusters):
-            self.priors.append(1/float(nClusters))
+            self.priors.append(1 / float(nClusters))
 
     def LogLikelihood(self, data):
-        loglikelihood = 0.0
-        for i in range(0, data.nRows):
-            expsum = list()
-            for cluster in range(self.nClusters):
-                expsum.append(self.priors[cluster] * self.LogProb(i, cluster, data))
-            loglikelihood += logExpSum(expsum)
-        return loglikelihood
+        return sum(logExpSum([math.log(self.priors[c]) + self.LogProb(i, c, data) for c in range(self.nClusters)])
+                   for i in range(data.nRows))
 
     # Compute marginal distributions of hidden variables
     def Estep(self):
-        for i in range(self.nClusters):
-            for j in range(self.data.nRows):
-                self.resp[i][j] = self.LogProb(j, i, self.data)
+        p = []
+        for row in range(self.data.nRows):
+            pi = [self.priors[c] * math.exp(self.LogProb(row, c, self.data)) for c in range(self.nClusters)]
+            tmp = sum(pi)
+            p.append([x / tmp for x in pi])
 
-        for j in range(self.data.nRows):
-            denm = logExpSum(self.resp[:, j])
-            for i in range(self.nClusters):
-                self.resp[i][j] = math.exp(self.resp[i][j] - denm)
+        return p
 
     # Update the parameter estimates
-    def Mstep(self):
-        # TODO: M-step
-        for i in range(self.nClusters):
-            total = np.zeros(self.data.nCols)
-            total2 = np.zeros(self.data.nCols)
-            denom = 0.0
-            p = list()
-            for j in range(self.data.nRows):
-                denom += self.resp[i][j]
-                total = np.add(total, np.multiply(self.data[j], self.resp[i][j]))
-
-            if denom == 0:
-                """"
-                Getting a divide by 0 error on denom
-                """
-                denom = 0.000001
-            total = total / (1.0 * denom)
-
-            for k in range(self.data.nCols):
-                p.append((total[k], self.parameters[i][k][1]))
-            self.parameters[i] = p
-
-            p = list()
-            means = [list(x) for x in zip(*self.parameters[i])][0]
-            for j in range(self.data.nRows):
-                xArr = np.subtract(self.data[j], means)
-                total2 = np.add(total2, (np.multiply(xArr, self.resp[i][j]) * xArr))
-            total2 = total2 / (1.0 * denom)
-
-            for k in range(self.data.nCols):
-                p.append((self.parameters[i][k][0], total2[k]))
-            self.parameters[i] = p
-            self.priors[i] = denom / (1.0 * self.data.nRows)
+    def Mstep(self, p):
+        for c in range(self.nClusters):
+            pr = [p[i][c] for i in range(self.data.nRows)]
+            self.priors[c] = sum(pr) / self.data.nRows
+            for col in range(self.data.nCols):
+                x = [self.data[i][col] for i in range(self.data.nRows)]
+                mean = self.parameters[c][col][0]
+                newMean = sum(pr[i] * x[i] for i in range(self.data.nRows)) / sum(pr)
+                newVar = sum(pr[i] * (x[i] - mean) ** 2 for i in range(self.data.nRows)) / sum(pr)
+                self.parameters[c][col] = (newMean, newVar)
 
     # Computes the probability that row was generated by cluster
     def LogProb(self, row, cluster, data):
-        # TODO: compute probability row i was generated by cluster k
-        epsilon = 1e-06
-        nCols = data.nCols
-        data = data[row]
-        prob = -1 * (nCols * 0.5) * math.log(2 * math.pi)
-
-        for i in range(nCols):
-            mean = self.parameters[cluster][i][0]
-            var = self.parameters[cluster][i][1]
-
-            if var == 0:
-                var = epsilon
-
-            prob += (-0.5 * np.log(var)) + (-0.5 * ((data[i] - mean) ** 2) / (var * 1.0))
-
-        return prob + np.log(self.priors[cluster])
+        param = self.parameters[cluster]
+        x = data[row]
+        return -0.5 * sum(math.log(param[i][1]) + (x[i] - param[i][0]) ** 2 / param[i][1] for i in range(data.nCols))
 
     def Run(self, maxsteps=100, testData=None):
-        # TODO: Implement EM algorithm
-        trainLikelihood = 0.0
-        testLikelihood = 0.0
-        printstep = 1
-        prevtrainLikelihood = trainLikelihood
-        for step in range(maxsteps):
-            self.Estep()
-            self.Mstep()
-            trainLikelihood = self.LogLikelihood(self.data)
-            p_change = ((trainLikelihood - prevtrainLikelihood)/trainLikelihood) * 100
-            if step % printstep == 0:
-                print("Iteration: ", step, " LogLikelihood: ", trainLikelihood, " Change", p_change, ' %')
-            prevtrainLikelihood = trainLikelihood
+        trainLikelihood = [self.LogLikelihood(self.data)]
+        testLikelihood = [self.LogLikelihood(testData)] if testData is not None else None
+        iter = 0
+        diff = 1000
+        while math.fabs(diff / trainLikelihood[-1]) > 0.001:
+            self.Mstep(self.Estep())
+            diff = trainLikelihood[-1]
+            trainLikelihood.append(self.LogLikelihood(self.data))
+            if testData is not None:
+                testLikelihood.append(self.LogLikelihood(testData))
+            diff = trainLikelihood[-1] - diff
+            print('Iteration: ', iter, 'Train: ', trainLikelihood[iter], 'Test: ', testLikelihood[iter])
+            iter = iter + 1
+        return trainLikelihood, testLikelihood
 
-        return (trainLikelihood, testLikelihood)
+    def getPrediction(self):
+        pr = self.Estep()
+        labels = []
+        for p in pr:
+            maxP = 0
+            for i in range(len(p)):
+                if p[i] > p[maxP]:
+                    maxP = i
+            labels.append(maxP + 1)
+        return labels
+
+
+def identifyClusters(labels, pred):
+    info = dict((k, dict((k, 0) for k in set(pred))) for k in set(labels))
+    for i in range(len(labels)):
+        info[labels[i]][pred[i]] += 1
+    return info
+
+
+def accuracy(info):
+    correct = 0.0
+    total = 0.0
+    for i in info:
+        for j in info[i]:
+            total += info[i][j]
+        correct += max(info[i][k] for k in info[i])
+
+    return correct / total
 
 
 if __name__ == "__main__":
     d = Data('wine.train')
-    if len(sys.argv) > 1:
-        e = EM(d, int(sys.argv[1]))
-    else:
-        e = EM(d, 3)
-    e.Run(100)
+    td = Data('wine.test')
+    labels = readTrue('wine-true.data')
+    #########################################################
+    while True:
+        try:
+            em = EM(d, 3)
+            trainLikelihood, testLikelihood = em.Run(100, td)
+            break
+        except:
+            pass
+    pred = em.getPrediction()
+    plt.title('Iterations vs log likelihood for training and test set')
+    plt.xlabel('# of iterations')
+    plt.ylabel('Log Likelihood')
+    plt.xticks(range(len(trainLikelihood)))
+    plt.plot(range(len(trainLikelihood)), trainLikelihood, 'bo', ms=3, ls='-', lw=1, label='Training set')
+    plt.plot(range(len(testLikelihood)), testLikelihood, 'gs', ms=3, ls='-', lw=1, label='Test set')
+    plt.legend()
+    plt.show()
+
+    ########################################################################
+    details = []
+    for i in range(10):
+        while True:
+            try:
+                em = EM(d, 3)
+                trainLikelihood, testLikelihood = em.Run(100, td)
+                details.append([trainLikelihood[0], trainLikelihood[-1], len(trainLikelihood),
+                                testLikelihood[0], testLikelihood[-1], len(testLikelihood)])
+                break
+            except:
+                pass
+
+    for row in details:
+        print(row)
+
+    em = EM(d, 3)
+    trainLikelihood, testLikelihood = em.Run(100)
+    pred = identifyClusters(labels, em.getPrediction())
+    print(pred)
+    ###########################################################################
+
+    ll = [[], []]
+    for nc in range(1, 11):
+        while True:
+            try:
+                em = EM(d, nc)
+                trainLikelihood, testLikelihood = em.Run(100, td)
+                ll[0].append(trainLikelihood[-1])
+                ll[1].append(testLikelihood[-1])
+                break
+            except:
+                pass
+    plt.title('Final log likelihood vs no. of clusters: ')
+    plt.xlabel('# of clusters')
+    plt.ylabel('Final log Likelihood')
+    plt.xticks(range(1 + len(ll[0])))
+    plt.plot(range(1, 1 + len(ll[0])), ll[0], 'bo', ms=3, ls='-', lw=1, label='Training set')
+    plt.plot(range(1, 1 + len(ll[1])), ll[1], 'gs', ms=3, ls='-', lw=1, label='Test set')
+    plt.legend()
+    plt.show()
